de2c89e6bb8b9bbfc446cd25083a409539dde2d4
duan <wddwyss@gmail.com>
2018-03-17 22:57:26 +0900

    http cache header

--- tabe/routes/api.py
+++ tabe/routes/api.py
@@ -7,13 +7,18 @@ from tabe.models import Restaurant, Tag
 bp = Blueprint('api', __name__)
 
 
+def with_cache(resp, timeout=3600):
+    resp.headers['Cache-Control'] = 'public, max-age={}'.format(timeout)
+    return resp
+
+
 @bp.route('/restaurants')
 def restaurants():
     args = request.args
     tag = args.get('tag', type=int)
     rv = Restaurant.query.options(db.joinedload(Restaurant.tags)) \
         .filter(Restaurant.tags.any(id=tag)).all()
-    return jsonify(rv)
+    return with_cache(jsonify(rv))
 
 
 @bp.route('/tags')
