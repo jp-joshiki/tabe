dda1b090f4c04ba9d0285c6dd1a7ca2f39a05832
duan <wddwyss@gmail.com>
2018-03-17 22:23:36 +0900

    Remove legacy

--- tabe/models/tag.py
+++ tabe/models/tag.py
@@ -56,21 +56,6 @@ TABELOG_2017_RAMEN_WEST = 10
 TABELOG_2017_SWEETS = 11
 TABELOG_2017_PAN = 12
 
-_tags = {
-    TABELOG_2017: ['食べログ 百名店 2017'],
-    TABELOG_2017_HAMBURGER: ['食べログ ハンバーガー 百名店 2017'],
-    TABELOG_2017_PIZZA: ['食べログ ピザ 百名店 2017'],
-    TABELOG_2017_PORK_CUTLET: ['食べログ とんかつ 百名店 2017'],
-    TABELOG_2017_CURRY: ['食べログ カレー 百名店 2017'],
-    TABELOG_2017_SOBA: ['食べログ そば 百名店 2017'],
-    TABELOG_2017_UDON: ['食べログ うどん 百名店 2017'],
-    TABELOG_2017_RAMEN_TOKYO: ['食べログ ラーメン 百名店 TOKYO 2017'],
-    TABELOG_2017_RAMEN_EASE: ['食べログ ラーメン 百名店 EAST 2017'],
-    TABELOG_2017_RAMEN_WEST: ['食べログ ラーメン 百名店 WEST 2017'],
-    TABELOG_2017_SWEETS: ['食べログ スイーツ 百名店 2017'],
-    TABELOG_2017_PAN: ['食べログ パン 百名店 2017'],
-}
-
 _GROUPED = [{
     'id': TABELOG_2017,
     'name_ja': '食べログ 百名店 2017',
