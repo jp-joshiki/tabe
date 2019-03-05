a1e2a35f1dc02e377164cabec037024282a7dc03
duan <wddwyss@gmail.com>
2019-03-05 23:15:24 +0900

    Add tabelog_100_top_2018

--- spiders/__main__.py
+++ spiders/__main__.py
@@ -3,6 +3,7 @@ import logging
 from tabe import create_basic_app
 from flask.cli import FlaskGroup
 from .tabelog_100_top_2017 import Tabelog100Top2017Spider
+from .tabelog_100_top_2018 import Tabelog100Top2018Spider
 from .tabelog_award_2017 import TabelogAward2017Spider
 from .tabelog_award_2018 import TabelogAward2018Spider
 from .tabelog_award_2019 import TabelogAward2019Spider
@@ -24,6 +25,7 @@ def cli():
 def run(spider):
     mapping = dict(
         tabelog_100_top_2017=Tabelog100Top2017Spider,
+        tabelog_100_top_2018=Tabelog100Top2018Spider,
         tabelog_award_2017=TabelogAward2017Spider,
         tabelog_award_2018=TabelogAward2018Spider,
         tabelog_award_2019=TabelogAward2019Spider,
--- spiders/tabelog_100_top_2018.py
+++ spiders/tabelog_100_top_2018.py
@@ -1,6 +1,57 @@
 from ._base import BaseSpider
+from tabe.consts import (
+    TABELOG_2018_TOP_100_YANKINIKU,
+    TABELOG_2018_TOP_100_PIZZA,
+    TABELOG_2018_TOP_100_OKONOMIYAKI,
+    TABELOG_2018_TOP_100_TONKATSU,
+    TABELOG_2018_TOP_100_CURRY,
+    TABELOG_2018_TOP_100_HAMBURGER,
+    TABELOG_2018_TOP_100_YAKITORI,
+    TABELOG_2018_TOP_100_UNAGI,
+    TABELOG_2018_TOP_100_UDON,
+    TABELOG_2018_TOP_100_SOBA,
+    TABELOG_2018_TOP_100_TOKYO_RAMEN,
+    TABELOG_2018_TOP_100_EAST_RAMEN,
+    TABELOG_2018_TOP_100_WEST_RAMEN,
+    TABELOG_2018_TOP_100_PAN,
+    TABELOG_2018_TOP_100_TOKYO_SWEETS,
+    TABELOG_2018_TOP_100_EAST_SWEETS,
+    TABELOG_2018_TOP_100_WEST_SWEETS,
+)
+
+BASE_URL = 'https://award.tabelog.com/hyakumeiten/2018/'
+
+ENTRIES = [
+    ('yakiniku/japan', TABELOG_2018_TOP_100_YANKINIKU),
+    ('pizza/japan', TABELOG_2018_TOP_100_PIZZA),
+    ('okonomiyaki/japan', TABELOG_2018_TOP_100_OKONOMIYAKI),
+    ('tonkatsu/japan', TABELOG_2018_TOP_100_TONKATSU),
+    ('curry/japan', TABELOG_2018_TOP_100_CURRY),
+    ('hamburger/japan', TABELOG_2018_TOP_100_HAMBURGER),
+    ('yakitori/japan', TABELOG_2018_TOP_100_YAKITORI),
+    ('unagi/japan', TABELOG_2018_TOP_100_UNAGI),
+    ('udon/japan', TABELOG_2018_TOP_100_UDON),
+    ('soba/japan', TABELOG_2018_TOP_100_SOBA),
+    ('ramen/tokyo', TABELOG_2018_TOP_100_TOKYO_RAMEN),
+    ('ramen/east', TABELOG_2018_TOP_100_EAST_RAMEN),
+    ('ramen/west', TABELOG_2018_TOP_100_WEST_RAMEN),
+    ('pan/japan', TABELOG_2018_TOP_100_PAN),
+    ('sweets/tokyo', TABELOG_2018_TOP_100_TOKYO_SWEETS),
+    ('sweets/east', TABELOG_2018_TOP_100_EAST_SWEETS),
+    ('sweets/west', TABELOG_2018_TOP_100_WEST_SWEETS),
+]
 
 
 class Tabelog100Top2018Spider(BaseSpider):
-    pass
+    target = './data/tabelog_100_top_2018.json'
 
+    def run(self):
+        for (path, tag) in ENTRIES:
+            url = f'{BASE_URL}/{path}'
+            r = self.fetch(url).html
+            items = r.find('li.hyakumeiten-rstlist__item')
+            for item in items:
+                a = item.find('a.hyakumeiten-rstlist__target', first=True)
+                target = a.attrs['href'].split('?')[0]
+                self.add(target, tag)
+        self.store()
